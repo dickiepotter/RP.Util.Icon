7f7b3452aada1e09babb80f7cc7abcc6a6debe1c
Richard Potter <dickiepotter@hotmail.com>
2015-09-29 13:02:30 +0100

    Initial Commit

---
+++ RP.Util.Icon/IconExtensions.cs
@@ -0,0 +1,83 @@
+﻿namespace RP.Util.Icon
+{
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.Windows;
+    using System.Windows.Interop;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+
+    public static class IconExtensions
+    {
+        #region Public Methods
+
+        public static Icon GetAssociatedIconLarge(this Process process)
+        {
+            string path = process.StartInfo.FileName;
+            if (string.IsNullOrEmpty(path))
+            {
+                try
+                {
+                    path = process.MainModule.FileName;
+                }
+                catch { }
+            }
+
+            Icon icon = null;
+            if (!string.IsNullOrEmpty(path))
+            {
+                icon = IconExtractor.GetAssociatedIconLarge(process.StartInfo.FileName);
+            }
+            return icon;
+        }
+
+        public static Icon GetAssociatedIconSmall(this Process process)
+        {
+            string path = process.StartInfo.FileName;
+            if (string.IsNullOrEmpty(path))
+            {
+                try
+                {
+                    path = process.MainModule.FileName;
+                }
+                catch { }
+            }
+
+            Icon icon = null;
+            if (!string.IsNullOrEmpty(path))
+            {
+                icon = IconExtractor.GetAssociatedIconSmall(process.StartInfo.FileName);
+            }
+            return icon;
+        }
+
+        public static ImageSource ToImageSource(this Icon icon)
+        {
+            ImageSource wpfBitmap = null;
+
+            if (icon != null)
+            {
+                Bitmap bitmap = icon.ToBitmap();
+                IntPtr hBitmap = bitmap.GetHbitmap();
+
+                wpfBitmap = Imaging.CreateBitmapSourceFromHBitmap
+                  (
+                    hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+
+                if (!IconExtractor.DeleteObject(hBitmap))
+                {
+                    throw new Win32Exception();
+                }
+            }
+
+            return wpfBitmap;
+        }
+
+        #endregion
+    }
+}
---
+++ RP.Util.Icon/IconExtractor.cs
@@ -0,0 +1,141 @@
+﻿namespace RP.Util.Icon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+
+    public static class IconExtractor
+    {
+        #region Constants
+
+        private const uint iconFlag = 0x100;
+        private const uint largeIconFlag = 0x0; // 'Large icon
+        private const uint smallIconFlag = 0x1; // 'Small icon
+
+        #endregion
+
+        #region Public Methods
+
+        public static int CountIcons(string path)
+        {
+            return ExtractIconEx(path, -1, null, null, 0);
+        }
+
+        public static System.Drawing.Icon GetAssociatedIconLarge(string path)
+        {
+            FileInfo info = new FileInfo();
+
+            SHGetFileInfo(path, 0, ref info, (uint)Marshal.SizeOf(info), iconFlag | largeIconFlag);
+
+            System.Drawing.Icon toReturn = null;
+
+            if (info.Icon != IntPtr.Zero)
+            {
+                toReturn = System.Drawing.Icon.FromHandle(info.Icon);
+            }
+
+            return toReturn;
+        }
+
+        public static System.Drawing.Icon GetAssociatedIconSmall(string path)
+        {
+            FileInfo info = new FileInfo();
+
+            SHGetFileInfo(path, 0, ref info, (uint)Marshal.SizeOf(info), iconFlag | smallIconFlag);
+
+            System.Drawing.Icon toReturn = null;
+
+            if (info.Icon != IntPtr.Zero)
+            {
+                toReturn = System.Drawing.Icon.FromHandle(info.Icon);
+            }
+
+            return toReturn;
+        }
+
+        public static System.Drawing.Icon[] GetLargeIcons(string path)
+        {
+            IntPtr[] icons;
+            IntPtr[] dummy;
+            GetIcons(path, out icons, out dummy);
+
+            List<System.Drawing.Icon> iconList = new List<System.Drawing.Icon>();
+
+            foreach (IntPtr ptr in icons)
+            {
+                System.Drawing.Icon icon = System.Drawing.Icon.FromHandle(ptr);
+                iconList.Add((System.Drawing.Icon)icon.Clone());
+                DestroyIcon(ptr);
+            }
+
+            return iconList.ToArray();
+        }
+
+        public static System.Drawing.Icon[] GetSmallIcons(string path)
+        {
+            IntPtr[] icons;
+            IntPtr[] dummy;
+            GetIcons(path, out dummy, out icons);
+
+            List<System.Drawing.Icon> iconList = new List<System.Drawing.Icon>();
+
+            foreach (IntPtr ptr in icons)
+            {
+                System.Drawing.Icon icon = System.Drawing.Icon.FromHandle(ptr);
+                iconList.Add((System.Drawing.Icon)icon.Clone());
+                DestroyIcon(ptr);
+            }
+
+            return iconList.ToArray();
+        }
+
+        #endregion
+
+        #region Protected/Internal Methods
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        internal static extern bool DeleteObject(IntPtr hObject);
+
+        #endregion
+
+        #region Private Methods
+
+        [DllImport("user32.dll", EntryPoint = "DestroyIcon", SetLastError = true)]
+        private static extern int DestroyIcon(IntPtr icon);
+
+        [DllImport("Shell32", CharSet = CharSet.Auto)]
+        private static extern int ExtractIconEx(string path, int iconIndex, IntPtr[] largeIcons, IntPtr[] smallIcons, int iconCount);
+
+        private static void GetIcons(string path, out IntPtr[] large, out IntPtr[] small)
+        {
+            int count = CountIcons(path);
+            large = new IntPtr[count];
+            small = new IntPtr[count];
+
+            ExtractIconEx(path, 0, large, small, count);
+        }
+
+        [DllImport("shell32.dll")]
+        private static extern IntPtr SHGetFileInfo(string path, uint fileAttributes, ref FileInfo info, uint size, uint flags);
+
+        #endregion
+
+        #region Nested type: FileInfo
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct FileInfo
+        {
+            public readonly IntPtr Icon;
+            public readonly IntPtr Index;
+            public readonly uint Attributes;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public readonly string DisplayName;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public readonly string TypeName;
+        };
+
+        #endregion
+    }
+}
